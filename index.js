5475a75294ff783f55f07a2390ae59adbcf8ff7a
davinci360 <jasonbrowntime@gmail.com>
2021-07-21 02:19:26 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+// it("has a 'newHeader' variable that points to node 'h1#victory'"
+newHeader.id = "victory"
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
